48ecc248d9972c34ed14f596f4088d0bf0ced55c
Mac <mac@Macs-MacBook-Pro.local>
2019-12-13 15:21:15 +0530

    Initial Commit

---
+++ TestAppForGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppForGit
+//
+//  Created by Mac on 13/12/19.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
